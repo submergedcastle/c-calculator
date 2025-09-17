8aae94cba35ae50ea2282cac512b39715fed74be
submergedcastle <lorenzoeliot.r@gmail.com>
2025-09-17 18:09:03 -0500

    Implemented division feature

--- calculator.c
+++ calculator.c
@@ -1,14 +1,14 @@
 int add(int a, int b);
 int subtract(int a, int b);
 int multiply(int a, int b);
-
+int divide(int a, int b);
 
 int main () {
 int a = 0; int b = 0;
 int result;
-printf ("Enter two numbers you would like to multiply:\n");
+printf ("Enter two numbers you would like to divide:\n");
 scanf ("%d %d", &a, &b);
-result = multiply(a, b);
+result = divide(a, b);
 printf ("Result is %d", result);
 scanf ("%d", &a); //Forces terminal to stay open to see sum, not meant for any use 
 }
@@ -32,4 +32,15 @@ int multiply(int a, int b){
     int product;
     product = a*b;
     return product;
+}
+
+//Division
+int divide(int a, int b){
+    int quotient;
+    if (b == 0) {
+        printf("Cannot divide by 0.\n");
+        return 0;
+    }
+    else quotient = a/b;
+    return quotient;
 }
