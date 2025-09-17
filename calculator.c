bd76a0bd6cfe1237403e2d881e060c61bf7caa98
submergedcastle <lorenzoeliot.r@gmail.com>
2025-09-17 18:01:00 -0500

    Implemented multiplication feature

--- calculator.c
+++ calculator.c
@@ -1,13 +1,14 @@
 int add(int a, int b);
 int subtract(int a, int b);
+int multiply(int a, int b);
+
 
 int main () {
 int a = 0; int b = 0;
-printf ("Enter two numbers you would like to subtract:\n");
-scanf ("%d %d", &a, &b);
-add(a, b);
 int result;
-result = subtract(a, b);
+printf ("Enter two numbers you would like to multiply:\n");
+scanf ("%d %d", &a, &b);
+result = multiply(a, b);
 printf ("Result is %d", result);
 scanf ("%d", &a); //Forces terminal to stay open to see sum, not meant for any use 
 }
@@ -24,4 +25,11 @@ int subtract(int a, int b){
     int difference;
     difference = a - b;
     return difference;
+}
+
+//Multiplication
+int multiply(int a, int b){
+    int product;
+    product = a*b;
+    return product;
 }
